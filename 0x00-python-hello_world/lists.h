f67dd6d2796f73fe37033d5a4be5b1ad23650ea6
rona4ld <ronaldchirchir5634@gmail.com>
2023-11-27 14:09:08 -0800

    this is my code

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -19,6 +19,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t sh);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
-int Check_cycle(listint_t *list);
+int check_cycle(listint_t *list);
 
 #endif /* LISTS_H*/
