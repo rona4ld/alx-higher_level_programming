cd5e339ade1b230665709141c324e265de31736a
rona4ld <ronaldchirchir5634@gmail.com>
2023-11-28 10:50:19 -0800

    this is my correction

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-#!/bin/python3
-for letter in "abcdefghijklmnopqrstuvwxyz":
-    if letter not in ['q', 'e']:
-        print(letter, end='')
+#!/usr/bin/python3
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) |= 'e':
+        print("{}".format(chr(letter)), end="")
